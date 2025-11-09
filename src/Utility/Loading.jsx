b429b5320ac8c069910ab214133fd9624c2200ec
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-11-10 00:19:05 +0600

    Loading implement and useHook created

---
+++ src/Utility/Loading.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full mx-auto animate-pulse h-[calc(100vh-60vh)]">
+      <lord-icon
+        src="https://cdn.lordicon.com/kixubvkn.json"
+        trigger="loop"
+        delay="100"
+        colors="primary:#632EE3,secondary:#9F62F2"
+        style={{ width: "120px", height: "120px" }}
+      ></lord-icon>
+
+      <h2 className="text-lg font-semibold text-center text-accent mt-3 tracking-wide">
+        Reviews in Progress...
+      </h2>
+
+      <p className="text-sm text-text-secondary mt-1">
+        Please wait while we load the best bites 🍔
+      </p>
+    </div>
+  );
+};
+
+export default Loading;
